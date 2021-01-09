30c103ec3cf1f4321d0f58613e597ceee2596633
Enrico Minack <github@enrico.minack.dev>
2021-01-09 20:05:58 +0100

    More tests

--- test/test_example.py
+++ test/test_example.py
@@ -6,6 +6,15 @@ class ExampleTest(unittest.TestCase):
     def test_example(self):
         self.assertEqual(True, True)
 
+    def test_more(self):
+        self.assertEqual(True, True)
+
+    def test_fail(self):
+        self.assertEqual(True, False)
+
+    def test_skip(self):
+        self.skipTest("not tested")
+
 
 if __name__ == '__main__':
     unittest.main()
