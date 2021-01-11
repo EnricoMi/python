2dc412f1740c918a755d1a13a6a42d5cd41de599
Enrico Minack <github@enrico.minack.dev>
2021-01-11 10:05:49 +0100

    Add conditional skip

--- test/test_example.py
+++ test/test_example.py
@@ -15,6 +15,11 @@ class ExampleTest(unittest.TestCase):
     def test_skip(self):
         self.skipTest("not tested")
 
+    def test_conditional_skip(self):
+        import sys
+        if sys.version_info[0:2] == (3, 6):
+            self.skipTest('not tested')
+
 
 if __name__ == '__main__':
     unittest.main()
