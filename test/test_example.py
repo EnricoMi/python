b42e74005357a84f8b189b7e63425cef88a7de2c
Enrico Minack <github@enrico.minack.dev>
2021-02-05 17:37:16 +0100

    Modify some skips

--- test/test_example.py
+++ test/test_example.py
@@ -4,20 +4,20 @@ import unittest
 class ExampleTest(unittest.TestCase):
 
     def test_pass(self):
-        self.assertEqual(True, True)
+        self.skipTest('not tested')
 
     def test_conditional_fail(self):
         import sys
         if sys.version_info[0:2] == (3, 8):
 	        self.assertEqual(False, True)
 
-    def test_skip(self):
-        self.skipTest('not tested')
-
     def test_conditional_skip(self):
         import sys
         if sys.version_info[0:2] == (3, 6):
             self.skipTest('not tested')
+
+    def test_more_skips(self):
+        self.skipTest('not tested')
 
 
 if __name__ == '__main__':
