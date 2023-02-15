30ccc9d43a8355b7e1861acc41832a53e657988b
Enrico Minack <github@enrico.minack.dev>
2023-02-15 11:49:14 +0100

    Fail one Python versions

--- test/test_example.py
+++ test/test_example.py
@@ -9,6 +9,11 @@ class ExampleTest(unittest.TestCase):
     def test_pass(self):
         self.assertEqual(True, True)
 
+    def test_conditional_fail(self):
+        import sys
+        if sys.version_info[0:2] == (3, 8):
+	        self.assertEqual(False, True)
+
     def test_skip(self):
         self.skipTest('not tested')
 
